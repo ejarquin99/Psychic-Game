1b93e32806b1cabe9e1c54026786ae8ea050e643
Aphrodisiac <edwinjarquin99@gmail.com>
2018-12-12 23:18:28 -0500

    Completed but without styling or personalization

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,17 +4,17 @@ var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l
     var losses = 0;
     var guessesLeft = 9;
     var guessesSoFar = [];
-    var computerGuess = [];
+    var computerGuess;
 
     var winsText = document.getElementById("wins-text");
     var lossesText = document.getElementById("losses-text");
     var guessesLeftText = document.getElementById("guessesLeft-text");
     var guessesSoFarText = document.getElementById("guessesSoFar-text");
+    var computersChoice = document.getElementById("computer-choice");
 
     window.onload = function() {
-        var compGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
-        computerGuess.push(compGuess);
-        console.log(computerGuess[0]);
+        computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+        console.log(computerGuess);
     };
 
     document.onkeyup = function(event) {
@@ -23,31 +23,30 @@ var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l
         console.log(guessesSoFar);
         
         
-        if ((userGuess === computerGuess[0]) && (guessesLeft >= 0))  {
+        if ((userGuess === computerGuess) && (guessesLeft > 0))  {
+            alert("Well Done! You guessed " + computerGuess + " correctly!")
             wins++;
             console.log(wins);
             guessesLeft = 9;
-            guessesSoFar.length = 0;
-            computerGuess.length = 0;
-            var compGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
-            computerGuess.push(compGuess);
+            computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+            guessesSoFar=[];
             console.log(computerGuess[0]);
-        } else if ((userGuess !== computerGuess[0]) && (guessesLeft > 0)) {
+        } else if ((userGuess !== computerGuess) && (guessesLeft > 0)) {
             guessesLeft = guessesLeft-1;
             console.log(guessesLeft);
         } else {
+            alert("Too bad! The letter was: " + computerGuess + " !");
             losses++;
             guessesLeft = 9;
-            guessesSoFar.length = 0;
-            computerGuess.length = 0;
-            var compGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
-            computerGuess.push(compGuess);
-            console.log(computerGuess[0]);
-        };
-    };
-
+            computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+            guessesSoFar=[];
+            console.log(computerGuess);
+        }
         winsText.textContent = "Wins: " + wins;
         lossesText.textContent = "Losses " + losses;
         guessesLeftText.textContent = "Guesses Left: " + guessesLeft;
         guessesSoFarText.textContent = "Guesses So Far: " + guessesSoFar;
+    };
     
+        
+        
